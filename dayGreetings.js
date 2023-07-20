f472915138dbfcc6cfef0fb445efa3a8b0dbcc00
Erica Hagle <ericahagle.dev@gmail.com>
2023-07-20 10:46:53 -0700

    Complete the challenge

---
+++ dayGreetings.js
@@ -0,0 +1,9 @@
+var synonyms = ["wonderful", "fantastic", "great", "ok"];
+
+var greetings = [];
+
+for (var i = 0; i < synonyms.length; i++) {
+    greetings.push(`Have a ${synonyms[i]} day!`);
+};
+
+console.log(greetings);
